5beffa177157f9cee34cd2b62f77f95af47fe8a9
njgibbon <njgibbon@outlook.com>
2021-02-14 22:43:52 +0000

    tidy up and most of main function but still not modular at all

---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+// FendConfig is data for Fend Configuration annotated to be pulled from .fend.yaml
+type FendConfig struct {
+	Skip struct {
+		File      []string `yaml:"file"`
+		Folder    []string `yaml:"folder"`
+		FileAll   []string `yaml:"file_all"`
+		FolderAll []string `yaml:"folder_all"`
+		Extension []string `yaml:"extension"`
+	} `yaml:"skip"`
+}
+
+// NewFendConfig returns a new decoded Config struct using .fend.yaml if it exists
+func newFendConfig() (*FendConfig, error) {
+	fendConfig := &FendConfig{}
+	file, err := os.Open(".fend.yaml")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	d := yaml.NewDecoder(file)
+	if err := d.Decode(&fendConfig); err != nil {
+		return nil, err
+	}
+	return fendConfig, nil
+}
+
+func main() {
+	fendConfig, err := newFendConfig()
+	if err != nil {
+		//Could not load .fend.yaml config file for some reason
+	}
+	//Decision to always skip the .git dir
+	fendConfig.Skip.FileAll = append(fendConfig.Skip.FileAll, ".git")
+	fmt.Print(fendConfig)
+	doIt(fendConfig)
+	fend(fendConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func doIt(cfg *FendConfig) {
+	fmt.Println(cfg.Skip.Extension)
+	fmt.Println(cfg.Skip.File)
+	fmt.Println(cfg.Skip.FileAll)
+	fmt.Println(cfg.Skip.Extension[0])
+}
+
+func fend(fendConfig *FendConfig) error {
+	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		//Main logic of whether to check
+		if info.IsDir() && (info.Name() == ".git") {
+			fmt.Println(path, "Skip .git")
+			return filepath.SkipDir
+		} else if info.Name() == "." {
+			fmt.Println(path, ". NA")
+		} else if info.IsDir() {
+			fmt.Println(path, "Dir NA")
+		} else if info.Size() == 0 {
+			fmt.Println(path, "Size 0 - False")
+		} else {
+			result, err := checkLineEnding(path)
+			if err != nil {
+				return err
+			}
+			fmt.Println(path, result)
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func checkLineEnding(fname string) (bool, error) {
+	posixNewLine := "\n"
+	file, err := os.Open(fname)
+	if err != nil {
+		return false, err
+	}
+	defer file.Close()
+	buf := make([]byte, 1)
+	stat, err := os.Stat(fname)
+	start := stat.Size() - 1
+	_, err = file.ReadAt(buf, start)
+	if err != nil {
+		return false, err
+	}
+	lastCharacter := string(buf)
+	if lastCharacter == posixNewLine {
+		return true, nil
+	}
+	return false, nil
+}
