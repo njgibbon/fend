4b6c2d29e9cb47369bb24bf560288804db494a51
njgibbon <njgibbon@outlook.com>
2021-03-03 18:02:21 +0000

    good pass at basic testing

--- pkg/scanner/scanner_test.go
+++ pkg/scanner/scanner_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Tests are coupled with the relative test/data.. directory
-func TestScan(t *testing.T) {
+func TestScanData0(t *testing.T) {
 	scanConfig := new(ScanConfig)
 	scanConfig.Skip.File = []string{"test/data-0/skip_file.txt"}
 	scanConfig.Skip.Dir = []string{"test/data-0/skip-dir"}
@@ -20,21 +20,52 @@ func TestScan(t *testing.T) {
 		t.Errorf("Not expecting Error.")
 	} else {
 		fmt.Println(scanResult)
+		fmt.Println(scanResult.Output())
+		if scanResult.Total != 8 {
+			t.Errorf("Total: Expected 8.")
+		}
 		if scanResult.Passed != 5 {
 			t.Errorf("Passed: Expected 5.")
 		}
+		if scanResult.Failed != 3 {
+			t.Errorf("Failed: Expected 3.")
+		}
+		if scanResult.SkippedDirs != 2 {
+			t.Errorf("Skipped Dirs: Expected 2.")
+		}
+		if scanResult.SkippedFiles != 3 {
+			t.Errorf("Skipped Files: Expected 3.")
+		}
+		if scanResult.Errors != 0 {
+			t.Errorf("Errors: Expected 0.")
+		}
+	}
+}
 
-		//TODO Rest
+func TestScanData1(t *testing.T) {
+	scanConfig := new(ScanConfig)
+
+	scanResult, err := Scan(scanConfig, "test/data-1")
+	if err != nil {
+		fmt.Println(err)
+		t.Errorf("Not expecting Error.")
+	} else {
+		fmt.Println(scanResult)
+		fmt.Println(scanResult.Output())
+		if scanResult.Total != 0 {
+			t.Errorf("Total: Expected 0.")
+		}
+		if scanResult.Passed != 0 {
+			t.Errorf("Passed: Expected 0.")
+		}
+		if scanResult.Failed != 0 {
+			t.Errorf("Failed: Expected 0.")
+		}
 	}
 }
 
 func TestScanUnknownPath(t *testing.T) {
 	scanConfig := new(ScanConfig)
-	scanConfig.Skip.File = []string{"test/data-0/skip_file.txt"}
-	scanConfig.Skip.Dir = []string{"test/data-0/skip-dir"}
-	scanConfig.Skip.FileAll = []string{"skip_file_all.txt"}
-	scanConfig.Skip.DirAll = []string{"skip-dir-all"}
-	scanConfig.Skip.Extension = []string{".skip", ".ignore"}
 
 	scanResult, err := Scan(scanConfig, "unknown-path")
 	if err != nil {
